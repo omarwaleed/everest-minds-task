cb5e44764abedb3821916a591f9ca1790cec570c
omarwaleed <omarpersonalprofile@gmail.com>
2016-10-26 01:17:07 +0200

    signin screen finished and added mongoose to node modules

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,22 @@
 
-
 module.exports = function(grunt) {
 
+	grunt.initConfig({
 
-	// grunt.registerTask('run' )
+		concat: {
+			js: {
+				src: ['node_modules/jquery/dist/jquery.min.js', 'node_modules/bootstrap/js/bootstrap.min.js',
+				 'node_modules/angular.min.js', 'node_modules/tinymce/tinymce.min.js'],
+				dest: 'public/javascripts/scripts.js'
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+
+	grunt.registerTask('combine', ['concat', 'uglify']);
 
 }
