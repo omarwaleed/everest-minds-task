6695fede4b6d11813a62221bd9d4d546fa582cb5
omarwaleed <omarpersonalprofile@gmail.com>
2016-11-06 17:06:24 +0200

    attempting to add sortable and recreated questions successfully in edit view

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules/bootstrap')));
+app.use(express.static(path.join(__dirname, 'node_modules/jquery-sortable')));
 
 app.use('/', routes);
 // app.use('/users', users);
